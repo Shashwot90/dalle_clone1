920fdc16f0ac07bf28fb77ce73773361b6918fb7
unknown <shashwotpradhan@gmail.com>
2024-08-11 14:08:31 +0545

    searchText

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,6 +5,9 @@ const RenderCards = ({ data, title}) => {
   if(data?.length > 0) {
     return data.map((post) => <Card key={post._id} {...post} />)
   }
+  return (
+    <h2 className="mt-5 font-bold text-[#6449ff] text-xl uppercase">{title}</h2>
+  )
 } 
 
 const Home = () => {
@@ -33,7 +36,13 @@ const Home = () => {
             {searchText && (
               <h2 className='font-medium text-[#666e75] text-xl mb-3'>Showing results for <span className='text-[#222328] '>{searchText}</span></h2>
             )}
-            <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3"></div>
+            <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
+              {searchText ? (
+
+              ) : (
+                
+              )}
+            </div>
           </>
         )}
       </div>
