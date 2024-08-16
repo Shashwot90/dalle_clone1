693929a44b56534f2f6d72d19493fe2d8c9ac7d3
unknown <shashwotpradhan@gmail.com>
2024-08-16 14:05:40 +0545

    imports

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,6 +1,8 @@
 import React, {useState} from 'react'
 import { useNavigate } from 'react-router-dom'
-
+import { preview } from '../assets'
+import { gentRandomPrompt } from '../utlis'
+import { FormField, Loader } from '../components'
 
 const CreatePost = () => {
   return (
