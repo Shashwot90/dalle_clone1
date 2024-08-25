61339c622431f15f5a25d3ee3df30e8d35f90a2e
unknown <shashwotpradhan@gmail.com>
2024-08-25 14:08:00 +0545

    form

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -21,7 +21,11 @@ const CreatePost = () => {
         <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
         <p className="mt-2 text-[#666e75] text-[16px] max-w-[500px]">Create imaginative and visually stunning images through DALL-E AI and share them with the community</p>      
       </div>
-      <form onSubmit={handleSubmit} className="mt-16 max-w-3xl"></form>
+      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+          <div className="flex flex-col gap-5">
+            
+          </div>
+      </form>
     </section>
   )
 }
