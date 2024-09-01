fc9429c3dd111c9cd3504624016aabdc709aaacf
unknown <shashwotpradhan@gmail.com>
2024-09-01 14:14:16 +0545

    props

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const FormField = () => {
+const FormField = ({ labelName, type, name, placeholder, value, handleChange, isSurpriseMe, handleSurpriseMe }) => {
   return (
     <div>
       <div className="flex items-center gap-2 mb-2">
