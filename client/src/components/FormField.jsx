9e91988e5de3ea36bd2f1ec96ffd9fe0bb3509ef
unknown <shashwotpradhan@gmail.com>
2024-09-06 15:51:24 +0545

    button class

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -13,7 +13,7 @@ const FormField = ({ labelName, type, name, placeholder, value, handleChange, is
           <button
           type='button'
           onClick={handleSurpriseMe}
-          className='font-semibold'>
+          className='font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black'>
             
           </button>
         )}
