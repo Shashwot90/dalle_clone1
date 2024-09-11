1cb0da3e52f9e624b37bd03da52d82837656442e
unknown <shashwotpradhan@gmail.com>
2024-09-11 07:51:30 +0545

    dalle class

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -26,7 +26,7 @@ const FormField = ({ labelName, type, name, placeholder, value, handleChange, is
         value={value}
         onChange={handleChange}
         required
-        className="bg-gray-50 border-gray-300"
+        className="bg-gray-50 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff]"
       />
     </div>
   )
