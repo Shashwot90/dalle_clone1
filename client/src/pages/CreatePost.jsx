6efc44d0a203b6f2c4b5f157cbb8cfbc3ca88f2a
unknown <shashwotpradhan@gmail.com>
2024-10-07 13:51:55 +0545

    class

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -82,7 +82,7 @@ const CreatePost = () => {
              </button>
           </div>
           <div className="mt-10">
-            <p className='mt-2'>Once you have created the image you want, you can share it with others in the community.</p>
+            <p className='mt-2 text-[#666e75] text-[14px]'>Once you have created the image you want, you can share it with others in the community.</p>
           </div>
       </form>
     </section>
