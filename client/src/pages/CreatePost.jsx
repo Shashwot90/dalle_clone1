701e1f53206444f7024c3b9506cd25846ab70498
unknown <shashwotpradhan@gmail.com>
2024-10-13 15:53:16 +0545

    button

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -83,7 +83,7 @@ const CreatePost = () => {
           </div>
           <div className="mt-10">
             <p className='mt-2 text-[#666e75] text-[14px]'>Once you have created the image you want, you can share it with others in the community.</p>
-            <button type='submit'>{loading ? 'Sharing...': 'Share with the community'}</button>
+            <button type='submit' className='mt-3 text-white '>{loading ? 'Sharing...': 'Share with the community'}</button>
           </div>
       </form>
     </section>
